d4d0a989c6f47f28747a2de6c1ddd41ae583f30e
Milan <pro@turtletv.fr>
2023-06-21 16:42:01 +0200

    ✨ Implement events contrller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
-import { PrismaService } from './services/prisma.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "./config/configuration";
+import { PrismaService } from "./services/prisma.service";
+import { MongoService } from "./services/mongo.service";
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [configuration],
-    isGlobal: true,
-  }), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    AuthModule,
+    EventsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, MongoService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,20 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaService } from './services/prisma.service';
-import { ServerHealthcheck } from './app.model';
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { PrismaService } from "./services/prisma.service";
+import { ServerHealthcheck } from "./app.model";
+import { MongoService } from "./services/mongo.service";
 
 @Injectable()
 export class AppService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mongo: MongoService
+  ) {}
   async healthcheck(): Promise<ServerHealthcheck> {
     return {
       server_status: HttpStatus.OK,
       prisma_status: await this.prismaHealthcheck(),
       timestamp: new Date(),
-    }
+    };
   }
 
   private async prismaHealthcheck(): Promise<number> {
@@ -21,5 +25,22 @@ export class AppService {
       // Log to logger service the error
       return HttpStatus.INTERNAL_SERVER_ERROR;
     }
+  }
+
+  async test(): Promise<object | PromiseLike<object>> {
+    return await this.mongo.buttonEvent.findMany();
+  }
+
+  async test2(): Promise<object | PromiseLike<object>> {
+    return await this.mongo.buttonEvent.create({
+      data: {
+        event_id: "test",
+        buttonContent: "test content",
+        pathname: "/test/path",
+        timestamp: Date.now(),
+        user: -1,
+        uuid: "my-uuid",
+      },
+    });
   }
 }
