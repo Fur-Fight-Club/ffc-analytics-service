9cbd50facd9ab10ac1c996616d840a7ee0820bdf
ZERRAI Sami <sami.zerrai@gmail.com>
2023-07-16 15:53:20 +0200

    Implement fireStore globaly

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,24 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import configuration from "./config/configuration";
 import { MongoService } from "./services/mongo.service";
 import { EventsModule } from "./events/events.module";
+import { FirestoreModule } from "./firestore/firestore.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
+    }),
+    FirestoreModule.forRoot({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        keyFilename: configService.get<string>("sa_key"),
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     EventsModule,
